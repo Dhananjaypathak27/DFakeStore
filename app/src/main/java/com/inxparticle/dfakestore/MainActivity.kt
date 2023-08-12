633ba0a98113c3f26592da8c5229ae4cc84e9802
dhananjaypathak27 <dj0027.dp@gmail.com>
2023-08-12 15:37:41 +0530

    bottom navigation bar added

--- app/src/main/java/com/inxparticle/dfakestore/MainActivity.kt
+++ app/src/main/java/com/inxparticle/dfakestore/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.inxparticle.dfakestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.viewModel = viewModel
+
+        val navController = this.findNavController(R.id.navHostFragment)
+
+        // Find reference to bottom navigation view
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+// Hook your navigation controller to bottom navigation view
+        navView.setupWithNavController(navController)
 
     }
 }
